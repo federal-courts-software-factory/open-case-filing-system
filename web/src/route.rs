e352a6287f1604e452a0d163932faf3685069dbc
Tyler Harpool <tharpool@jackhenry.com>
2024-02-05 07:25:15 +0000

    Clean up dirty commits

--- web/src/route.rs
+++ web/src/route.rs
@@ -3,8 +3,5 @@ use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 
 pub fn create_router() -> Router {
-    Router::new().nest_service(
-        "/",
-        ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
-    )
+    Router::new().route_service("/", ServeFile::new("dist/index.html"))
 }
