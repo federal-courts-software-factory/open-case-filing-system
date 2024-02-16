31e6339af1d6237f7cffc0c13a9f85f6e814f8cd
Tyler Harpool <tharpool@jackhenry.com>
2024-02-15 23:11:53 -0600

    Bump rust version back to 1.72

--- web/src/route.rs
+++ web/src/route.rs
@@ -4,4 +4,8 @@ use tower_http::services::ServeFile;
 
 pub fn create_index() -> Router {
     Router::new().route_service("/", ServeFile::new("dist/index.html"))
+    .nest_service(
+        "/assets",
+        ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+    );
 }
